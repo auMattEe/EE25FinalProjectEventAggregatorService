ec40dfff5ee6f38fef1bbe783f582c9eb53f400d
auMattEe <matt.eaton85@gmail.com>
2023-10-22 09:20:30 +0300

    Initial Commit

---
+++ src/main/java/com/example/EE25FinalProjectEventAggregatorService/Ee25FinalProjectEventAggregatorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.EE25FinalProjectEventAggregatorService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ee25FinalProjectEventAggregatorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ee25FinalProjectEventAggregatorServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/EE25FinalProjectEventAggregatorService/Ee25FinalProjectEventAggregatorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EE25FinalProjectEventAggregatorService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ee25FinalProjectEventAggregatorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
